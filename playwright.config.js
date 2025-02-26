09880deefeb24bb35f4d4af2c70464b543af07cd
Daniel Siegl <daniel.siegl@lieberlieber.com>
2025-02-26 14:39:46 +0100

    [#6] Create unittests for js
    As the node.js webserver is not working I am using phyton webserver now.

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,19 @@
-module.exports = {
+import { defineConfig } from '@playwright/test';
+export default defineConfig({
   testDir: 'tests',
-  
   timeout: 30000,
   use: {
     headless: true,
     actionTimeout: 0,
     trace: 'on',
-  }
-};
+  },
+  // Run your local dev server before starting the tests
+  // webServer: {
+  //   command: 'npm run start && npx wait-on http://127.0.0.1:8000',
+  //   port: 8000,
+  //   reuseExistingServer: !process.env.CI,
+  //   stdout: 'ignore',
+  //   stderr: 'pipe',
+  //   timeout: 120000,
+  // },
+});
