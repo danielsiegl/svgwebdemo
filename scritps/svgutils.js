43b8b03e748c2839a6e85fe2631cc4cfadcdaf7b
Daniel Siegl <daniel.siegl@syntevo.com>
2025-03-01 00:17:48 +0100

    [#23] Prepare js for Unitests
    Add JavaScript functions to move a spaceship SVG left and right and
    change the color of its window. Update the HTML to reflect the changes
    in button functionality and fix a typo in the script reference.
    Introduce a new utility module for handling SVG manipulations.

--- scritps/svgutils.js
+++
@@ -1,7 +0,0 @@
-export function moveSection(idStr, xOffset, yOffset) {
-    var domElemnt = document.getElementById(idStr);
-    if (domElemnt) {
-        var transformAttr = ' translate(' + xOffset + ',' + yOffset + ')';
-        domElemnt.setAttribute('transform', transformAttr);
-    }
-}
